cb82f7986dfa6ab5f8d986070c893a3d49597cf5
davidsimas <david.simas81@gmail.com>
2023-02-16 20:45:05 -0300

    Exercicio01: pegando a posição do mouse na tela

---
+++ robo01.py
@@ -0,0 +1,10 @@
+# Importando a biblioteca Pyautogui e atribuindo um apelido direto
+import pyautogui as p
+
+
+# Usando p para criar um time de execução
+p.sleep(5)
+
+
+# Print position ira mostrar as coordenadas
+print(p.position())
