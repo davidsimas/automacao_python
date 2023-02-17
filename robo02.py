9f9935ed8a6c5edbeaec4e6aabf893b817dbdcad
davidsimas <david.simas81@gmail.com>
2023-02-17 18:45:43 -0300

    Subindo exercicios 02 e 03

---
+++ robo02.py
@@ -0,0 +1,32 @@
+# Importando a biblioteca Pyautogui e atribuindo um apelido direto
+import pyautogui as p
+
+
+#ele recebe uma cordenada x e uma cordenada Y movendo o mause para devidas cordenadas
+p.moveTo(x=44, y=451, duration=1)
+p.click(x=44, y=451)
+p.click(x=44, y=451)
+
+p.moveTo(x=816, y=493, duration=1)
+p.click(x=816, y=493)
+p.click(x=816, y=493)
+
+p.moveTo(x=331, y=23, duration=1)
+p.click(x=331, y=23)
+p.click(x=331, y=23)
+
+p.moveTo(x=429, y=68, duration=1)
+p.click(x=429, y=68)
+p.typewrite('python')
+p.hotkey('enter')
+p.sleep(6)
+p.click(x=592, y=18)
+
+p.moveTo(x=331, y=23, duration=1)
+p.click(x=331, y=23)
+p.click(x=331, y=23)
+
+p.moveTo(x=429, y=68, duration=1)
+p.click(x=429, y=68)
+p.typewrite('julio cocorico')
+p.hotkey('enter')
